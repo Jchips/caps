b9692d8e4ec1ddf55f4b18440d18b43e66966fb8
Jchips <20979096+Jchips@users.noreply.github.com>
2024-02-21 15:31:08 -0800

    added payload constructor for different orders

--- vendor/index.js
+++ vendor/index.js
@@ -8,16 +8,23 @@ let chance = new Chance();
 
 const vendor = (storeName) => {
 
-  const payload = {
-    store: storeName,
-    orderId: chance.guid(),
-    customer: chance.name(),
-    address: chance.address(),
-  };
+  function Payload(storeName) {
+    this.storeName = storeName;
+    this.orderId = chance.guid();
+    this.customer = chance.name();
+    this.address = chance.address();
+  }
+
+  // const payload = {
+  //   store: storeName,
+  //   orderId: chance.guid(),
+  //   customer: chance.name(),
+  //   address: chance.address(),
+  // };
 
   setInterval(() => {
     console.log('----------new package--------');
-    eventEmitter.emit('pickup', payload);
+    eventEmitter.emit('pickup', new Payload(storeName));
   }, 5000);
 
   eventEmitter.on('delivered', (payload) => {
